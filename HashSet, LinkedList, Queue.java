8e69a10d72892213d2b9dd2893e12e82013beb3f
Cheng Liang Yu <chengly3@uci.edu>
2019-04-21 22:08:25 -0700

    Create HashSet, LinkedList, Queue.java

---
+++ HashSet, LinkedList, Queue.java
@@ -0,0 +1,31 @@
+#365. Water and Jug Problem
+class Solution {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if (z < 0 || z > x + y) {
+            return false;
+        }
+        Set<Integer> set = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        while (!q.isEmpty()) {
+            int n = q.poll();
+            if (n + x <= x + y && set.add(n + x)) {
+                q.offer(n + x);
+            } 
+            if (n + y <= x + y && set.add(n + y)) {
+                q.offer(n + y);
+            } 
+            if (n - x >= 0 && set.add(n - x)) {
+                q.offer(n - x);
+            } 
+            if (n - y >= 0 && set.add(n - y)) {
+                q.offer(n - y);
+            }
+            if (set.contains(z)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
