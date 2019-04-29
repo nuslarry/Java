3de2b379c7e7f6655eabec144d93e1c61b269ca7
Cheng Liang Yu <chengly3@uci.edu>
2019-04-29 09:55:46 -0700

    Update java備忘錄.java

--- java備忘錄.java
+++ java備忘錄.java
@@ -1,1 +1,13 @@
-arr[] = {10, 3, 5, 10, 3, 5, 10};
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
+
+
+int arr[] = {10, 3, 5, 10, 3, 5, 10}; //declaration
+public static void main(String []args) //as function input
+  
+  
+
